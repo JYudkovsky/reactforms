17cd984ff8b66bb7dc27d68a7bab0bfa619da5c7
josh <josh@joshubuntu.myguest.virtualbox.org>
2023-10-13 14:57:33 -0400

    Added form and labels to signupform component

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -11,4 +11,15 @@ export default function SignUpForm() {
 
 
     return <h2> Sign Up!</h2>;
+
+    <form>
+        <label>
+            Username: <input />
+        </label>
+        <label>
+            Password: <input />
+        </label>
+        <button>Submit</button>
+
+    </form>
 }
